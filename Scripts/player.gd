467ee060162efc09701e71506f64bc6cf5f3798e
AndyDrewE <easleyandrewp@gmail.com>
2025-08-15 12:13:40 -0500

    Improved Movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 150.0
-const JUMP_VELOCITY = -500.0
+@export var SPEED = 150.0
+@export var JUMP_VELOCITY = -500.0
 const MAX_GRAPPLE_DIST = 700.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -44,15 +44,11 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y += JUMP_VELOCITY
-	
-	#Normal gravity on the way up
-	if velocity.y < 0:
-		velocity.y += gravity * delta
-	#speedy on the way down
-	else:
-		velocity.y += 3 * gravity *delta
 
+	if Input.is_action_just_released("ui_accept") and not is_on_floor() and velocity.y < 0:
+		velocity.y = -velocity.y
 
+	velocity.y += gravity * delta
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
