03a746609a302e683a5ec75e4d56c5fc20dfcbed
AndyDrewE <easleyandrewp@gmail.com>
2025-08-17 16:02:53 -0500

    Test level edits, added terminal velocity
    Test level edits, added terminal velocity to player, Need to tweak camera and velocity

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
+## TODO: Dynamic camera smoothing as player gets faster
 
 @export var SPEED = 150.0
 @export var JUMP_VELOCITY = -500.0
+@export var TERMINAL_VELOCITY = 5000.0
 const MAX_GRAPPLE_DIST = 700.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -44,11 +46,16 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y += JUMP_VELOCITY
-
+	#If player releases jump, reverse velocity and slow it down
 	if Input.is_action_just_released("ui_accept") and not is_on_floor() and velocity.y < 0:
 		velocity.y = -velocity.y*0.5
 
-	velocity.y += gravity * delta
+	#Cap vertical velocity
+	if velocity.y >= TERMINAL_VELOCITY:
+		velocity.y = TERMINAL_VELOCITY
+	else:
+		velocity.y += gravity * delta
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
