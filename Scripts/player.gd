e509cdea6d2907523b8e5653c7bd8ff3c42803e3
AndyDrewE <easleyandrewp@gmail.com>
2025-08-18 09:10:11 -0500

    Update player.gd

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -11,38 +11,15 @@ const MAX_GRAPPLE_DIST = 700.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-#Grapple Raycast Object
+#Grappling hook
 @onready var grapple_raycast = $GrappleRay
 var grappling = false
 
 func _input(event):
-	if event.is_action_pressed("ui_shoot"):
-		# Aim from the RayCast2D's origin toward the mouse, in LOCAL space.
-		var to_mouse_local = grapple_raycast.to_local(get_global_mouse_position())
-		# Clamp to a max distance so the ray isn't absurdly long.
-		if to_mouse_local.length() > MAX_GRAPPLE_DIST:
-			to_mouse_local = to_mouse_local.normalized() * MAX_GRAPPLE_DIST
-		grapple_raycast.target_position = to_mouse_local
-		
-		grapple_raycast.enabled = true
-		grappling = true
+	pass
 
 func _physics_process(delta):
-	if grappling:
-		grapple_raycast.force_raycast_update()
-
-		if grapple_raycast.is_colliding():
-			var hit_point = grapple_raycast.get_collision_point()
-			var hit_body = grapple_raycast.get_collider()
-			print("Grapple hit:", hit_body, "at", hit_point)
-
-			# turn off ray until next shot
-			grapple_raycast.enabled = false
-			grappling = false
-	
-	if grapple_raycast.is_colliding():
-		var collision_point = grapple_raycast.get_collision_point()
-		print("Collision point: ", collision_point)
+	grapple()
 	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -66,3 +43,8 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func grapple():
+	grapple_raycast.target_position = get_global_mouse_position()
+	if Input.is_action_just_pressed("ui_shoot"):
+		print(grapple_raycast.target_position)
