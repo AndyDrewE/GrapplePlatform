0320328608c7858b9e236ca0904087c0afbb3cf4
AndyDrewE <easleyandrewp@gmail.com>
2025-08-18 21:41:33 -0500

    Grappling hook mainly done
    Grappling hook mainly done, needs a lot of tweaking to make it fun to use

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,13 +1,15 @@
 extends CharacterBody2D
 
 ## TODO: Dynamic camera smoothing as player gets faster
-## TODO: Grappling hook
+## TODO: Grappling hook: keep tangential velocity when releasing the hook
+## TODO: Some sort of UI to tell you where the grappling hook will land
+## TODO: Grappling hook very hard to control with kbm
 
 @export var SPEED = 150.0
 @export var JUMP_VELOCITY = -500.0
 @export var TERMINAL_VELOCITY = 1000.0
 @export var REEL_SPEED = 300.0
-@export var TANGENTIAL_ACCEL = 1200.0
+@export var TANGENTIAL_ACCEL = 500.0
 const MAX_GRAPPLE_DIST = 700.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -15,6 +17,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 #Grappling hook
 @onready var grapple_raycast = $GrappleRay
+@onready var grapple_rope = $GrappleRay/GrappleRope
 var grappling = false
 var grapple_point := Vector2.ZERO
 var rope_length := 0.0
@@ -23,20 +26,16 @@ func _input(event):
 	pass
 
 func _physics_process(delta):
-	
-	#Handle Grappling
-	if Input.is_action_just_pressed("ui_shoot"):
-		if !grappling:
-			grapple()
-		else:
-			release_grapple()
-	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y += JUMP_VELOCITY
 	#If player releases jump, reverse velocity and slow it down
-	if Input.is_action_just_released("ui_accept") and not is_on_floor() and velocity.y < 0:
-		velocity.y = -velocity.y*0.5
+	if Input.is_action_just_released("ui_accept") and not is_on_floor():
+		if grappling:
+			release_grapple()
+		else:
+			if velocity.y < 0:
+				velocity.y = -velocity.y*0.5
 
 	#Cap vertical velocity
 	if velocity.y >= TERMINAL_VELOCITY:
@@ -53,16 +52,16 @@ func _physics_process(delta):
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 	else:
 		# --- Swing physics (rope constraint) ---
-		var r := global_position - grapple_point
+		var r = global_position - grapple_point
 		var dist = max(r.length(), 0.001)
 		var dir = r / dist                     # from anchor -> player
-		var tangent := Vector2(dir.y, dir.x)   # 90° to the rope
-
+		var tangent = Vector2(dir.y, dir.x)   # 90° to the rope
+	
 		if Input.is_action_pressed("grapple_reel_in"):
-			rope_length = max(40.0, rope_length - REEL_SPEED * delta)
-		if Input.is_action_pressed("grapple_reel_out"):
+			rope_length = max(10.0, rope_length - REEL_SPEED * delta)
+		elif Input.is_action_pressed("grapple_reel_out"):
 			rope_length = min(MAX_GRAPPLE_DIST, rope_length + REEL_SPEED * delta)
-
+			
 		# Pump the swing with left/right as tangential acceleration
 		velocity += tangent * (direction * TANGENTIAL_ACCEL * delta)
 
@@ -71,6 +70,21 @@ func _physics_process(delta):
 		if dist >= rope_length and v_radial > 0.0:
 			velocity -= dir * v_radial
 	move_and_slide()
+	
+	
+	#Handle Grappling
+	if Input.is_action_just_pressed("ui_shoot"):
+		if !grappling:
+			grapple()
+		else:
+			release_grapple()
+	
+	if grappling:
+		var r2 := global_position - grapple_point
+		var d2 := r2.length()
+		if d2 > rope_length:
+			global_position = grapple_point + r2.normalized() * rope_length
+		grapple_rope.points = [to_local(self.position), to_local(grapple_point)]
 
 func grapple():
 	# Aim the ray in LOCAL space, then cast once
@@ -85,14 +99,17 @@ func grapple():
 		grapple_point = grapple_raycast.get_collision_point()
 		rope_length = (global_position - grapple_point).length()
 		grappling = true
-		print("Grapple attached at: ", grapple_point)
+		
+		
 	# turn the node off regardless; we only needed it to find the hit
 	grapple_raycast.enabled = false
 
 func release_grapple():
 	grappling = false
+	grapple_rope.points = []
 
 func get_grapple_direction():
 	var direction = self.position.direction_to(get_global_mouse_position())
 	var target_position = (direction * MAX_GRAPPLE_DIST) + self.position
 	return target_position
+
